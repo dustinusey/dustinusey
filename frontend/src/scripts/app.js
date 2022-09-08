d957185ff0b940b8e90a6322baddec7d3e4a0c4e
dusey <dustinusey@me.com>
2022-09-08 14:27:39 -0500

    hero particals

--- frontend/src/scripts/app.js
+++ frontend/src/scripts/app.js
@@ -7,3 +7,33 @@ const handleHamAnimations = () => {
 }
 hamMenu.addEventListener('click', handleHamAnimations);
 offScreenNav.addEventListener('click', handleHamAnimations);
+
+
+const particalBoard = document.querySelector('.partical-board');
+const config = {
+  particalCount: 5000,
+}
+for (let i = 0; i < config.particalCount; i++) {
+  createPartical();
+  positionPartical();
+}
+function createPartical() {
+  let partical = document.createElement('div');
+  partical.classList.add('partical');
+  particalBoard.appendChild(partical);
+}
+function positionPartical(index) {
+  let particals = document.querySelectorAll('.partical');
+  particals.forEach(partical => {
+    let randomTop = Math.floor(Math.random() * (-500 - 500) + 500);
+    partical.style.top = `${randomTop}%`
+    
+    let randomLeft = Math.floor(Math.random() * (-500 - 500) + 500);
+    partical.style.left = `${randomLeft}%`
+
+    let randomDuration = Math.floor(Math.random() * 20);
+    let randomDelay = Math.floor(Math.random() * 10);
+
+    partical.style.animation = `particals ${randomDuration}s ${randomDelay}s ease infinite`
+  });
+}
