998e26a804513442ae1bc5ade8cbf9f6eb899eb0
dusey <dustinusey@me.com>
2022-09-08 15:08:05 -0500

    update performance w particals

--- frontend/src/scripts/app.js
+++ frontend/src/scripts/app.js
@@ -11,7 +11,7 @@ offScreenNav.addEventListener('click', handleHamAnimations);
 
 const particalBoard = document.querySelector('.partical-board');
 const config = {
-  particalCount: 150,
+  particalCount: 200,
 }
 for (let i = 0; i < config.particalCount; i++) {
   createPartical();
@@ -24,15 +24,18 @@ function createPartical() {
 }
 function positionPartical(index) {
   let particals = document.querySelectorAll('.partical');
+  for (let i = 0; i < particals.length / 2; i++) {
+    particals[i].classList.add('mobile');
+  }
   particals.forEach(partical => {
-    let randomTop = Math.floor(Math.random() * (-500 - 500) + 500);
+    let randomTop = Math.floor(Math.random() * (-700 - 700) + 700);
     partical.style.top = `${randomTop}%`
     
-    let randomLeft = Math.floor(Math.random() * (-500 - 500) + 500);
+    let randomLeft = Math.floor(Math.random() * (-700 - 700) + 700);
     partical.style.left = `${randomLeft}%`
 
-    let randomDuration = Math.floor(Math.random() * 20);
-    let randomDelay = Math.floor(Math.random() * 10);
+    let randomDuration = Math.floor(Math.random() * 50);
+    let randomDelay = Math.floor(Math.random() * 1);
 
     partical.style.animation = `particals ${randomDuration}s ${randomDelay}s ease infinite`
   });
